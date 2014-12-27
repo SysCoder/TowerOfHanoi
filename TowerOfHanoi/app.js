574d48cf7aa49adeb26fabfed11b8c2bea9238c1
Marcellus Pelcher <pelcher@gmail.com>
2014-12-27 06:16:24 +0000

    Add pegs

--- TowerOfHanoi/app.js
+++ TowerOfHanoi/app.js
@@ -4,9 +4,24 @@ var Q = Quintus()
         .include("Sprites, Scenes, Input, 2D, Touch, UI")
         .setup({ maximize: true })
         .controls().touch()
-   
+
+Q.Sprite.extend("Peg", {
+  init: function(p) {
+    this._super(p, { x: 100, y: 50, w:100 * 2, h:20, color: "black"});
+    this.p.y += this.p.w;
+  },
+  draw: function(ctx) {
+     this._super(ctx);
+     ctx.rect(-(this.p.h/2), -this.p.w, this.p.h, this.p.w);
+     ctx.fillStyle = 'black';
+     ctx.fill();
+  }
+});
+
 Q.scene("level1",function(stage) {
-  stage.insert(new Q.Sprite({ x:50, y:50, w:50 * 2, h:50 + 5, color: "black"}));
+  stage.insert(new Q.Peg({x: 300}));
+  stage.insert(new Q.Peg({x: 550}));
+  stage.insert(new Q.Peg({x: 800}));
 });
 
 
